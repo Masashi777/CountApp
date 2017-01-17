25cf83335fe95a2dd86daf94e745d91ce983c203
AkitoshiHashizume <aki.application0101@gmail.com>
2017-01-17 20:25:08 +0900

    initial commit

---
+++ app/src/main/java/com/example/akitoshihashizume/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.akitoshihashizume.myapplication;
+
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int number = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText(String.valueOf(number));
+    }
+
+    public void add(View v){
+
+        number++;
+        textView.setText(String.valueOf(number));
+    }
+}
